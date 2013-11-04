882afacad16e7fc175257d3453ecd3d9d2d2cea9
Brian G. Butterfield <brian@butter-field.net>
2013-11-04 14:10:44 -0700

    Some basic cleanup in prep for lecture.

--- Example10.ContentProvider/src/edu/sdsmt/cs492/example10/contentprovider/model/Contact.java
+++ Example10.ContentProvider/src/edu/sdsmt/cs492/example10/contentprovider/model/Contact.java
@@ -38,8 +38,21 @@ public class Contact implements Parcelable
 	{
 	}
 
-	public Contact(Parcel parcel)
+	public Contact(final Cursor cursor)
 	{
+		// Public constructor used by ContactModel to create a Contact
+		// object from the passed database cursor.
+		this.ContactID = cursor.getLong(cursor.getColumnIndex(Contact.COLUMN_ID));
+		this.Name = cursor.getString(cursor.getColumnIndex(Contact.COLUMN_CONTACT_NAME));
+		this.Phone = cursor.getString(cursor.getColumnIndex(Contact.COLUMN_PHONE));
+		this.Email = cursor.getString(cursor.getColumnIndex(Contact.COLUMN_EMAIL));
+		this.Street = cursor.getString(cursor.getColumnIndex(Contact.COLUMN_STREET));
+		this.City = cursor.getString(cursor.getColumnIndex(Contact.COLUMN_CITY));
+	}
+
+	private Contact(Parcel parcel)
+	{
+		// Private constructor used by Creator specific to Parcelable interface.
 		ContactID = parcel.readLong();
 		Name = parcel.readString();
 		Phone = parcel.readString();
@@ -48,15 +61,46 @@ public class Contact implements Parcelable
 		City = parcel.readString();
 	}
 
-	public Contact(final Cursor cursor)
+	@Override
+	public int describeContents()
 	{
-		this.ContactID = cursor.getLong(cursor.getColumnIndex(Contact.COLUMN_ID));
-		this.Name = cursor.getString(cursor.getColumnIndex(Contact.COLUMN_CONTACT_NAME));
-		this.Phone = cursor.getString(cursor.getColumnIndex(Contact.COLUMN_PHONE));
-		this.Email = cursor.getString(cursor.getColumnIndex(Contact.COLUMN_EMAIL));
-		this.Street = cursor.getString(cursor.getColumnIndex(Contact.COLUMN_STREET));
-		this.City = cursor.getString(cursor.getColumnIndex(Contact.COLUMN_CITY));
+		// There are cases where you need to use the constant CONTENTS_FILE_DESCRIPTOR.
+		// Majority of the time, just return 0.
+		return 0;
 	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags)
+	{
+		// Write values to save to Parcel.
+		//
+		// Parcel class supports writing of Primitive types (and String) or
+		// arrays or lists of Primitive types.
+		//
+		// or, the Parcel class supports writing other Parcelable classes.
+		dest.writeLong(ContactID);
+		dest.writeString(Name);
+		dest.writeString(Phone);
+		dest.writeString(Email);
+		dest.writeString(Street);
+		dest.writeString(City);
+	}
+
+	public static final Parcelable.Creator<Contact> CREATOR = new Parcelable.Creator<Contact>()
+	{
+		public Contact createFromParcel(Parcel pc)
+		{
+			// Passes in the unmarshalled Parcel and using the private
+			// contructor returns the object.
+			return new Contact(pc);
+		}
+
+		public Contact[] newArray(int size)
+		{
+			// Supports array of parcelable objects.
+			return new Contact[size];
+		}
+	};
 
 	public ContentValues getContentValues()
 	{
@@ -66,37 +110,7 @@ public class Contact implements Parcelable
 		values.put(Contact.COLUMN_EMAIL, Email);
 		values.put(Contact.COLUMN_STREET, Street);
 		values.put(Contact.COLUMN_CITY, City);
-
+	
 		return values;
 	}
-
-	@Override
-	public int describeContents()
-	{
-		return 0;
-	}
-
-	@Override
-	public void writeToParcel(Parcel dest, int flags)
-	{
-		dest.writeLong(ContactID);
-		dest.writeString(Name);
-		dest.writeString(Phone);
-		dest.writeString(Email);
-		dest.writeString(Street);
-		dest.writeString(City);
-	}
-
-	public static final Parcelable.Creator<Contact> Creator = new Parcelable.Creator<Contact>()
-	{
-		public Contact createFromParcel(Parcel pc)
-		{
-			return new Contact(pc);
-		}
-
-		public Contact[] newArray(int size)
-		{
-			return new Contact[size];
-		}
-	};
 }
