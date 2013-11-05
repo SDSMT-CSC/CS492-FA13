8f630a943711ca9942e45317a53aa3a4cec591f8
Brian G. Butterfield <brian@butter-field.net>
2013-11-05 15:42:26 -0700

    Reorder logic for better readability.

--- Example18.Notifications/src/edu/sdsmt/cs492/example18/notifications/MainActivity.java
+++ Example18.Notifications/src/edu/sdsmt/cs492/example18/notifications/MainActivity.java
@@ -53,6 +53,14 @@ public class MainActivity extends Activity
 			
 			NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 			
+			// Build a PendingIntent, so when the user clicks on the Notification
+			// from the drawer, it will display this Activity.
+			PendingIntent pendingIntent = PendingIntent.getActivity(getApplication(), 
+					                                                0, 
+					                                                new Intent(v.getContext(), MainActivity.class),  
+					                                                PendingIntent.FLAG_UPDATE_CURRENT);
+						
+						
 			// Define the notification that will be displayed to the user in two forms:
 			// 1. via small icon (STAR) in the notification area or status bar and
 			// 2. via the notification drawer when the user pulls it down.
@@ -62,14 +70,6 @@ public class MainActivity extends Activity
 			builder.setContentText("Displaying REGULAR activity.");
 			builder.setSubText("Are you paying attention?");
 			builder.setNumber(++counter);
-			
-			// Build a PendingIntent, so when the user clicks on the Notification
-			// from the drawer, it will display this Activity.
-			PendingIntent pendingIntent = PendingIntent.getActivity(getApplication(), 
-					                                                0, 
-					                                                new Intent(v.getContext(), MainActivity.class),  
-					                                                PendingIntent.FLAG_UPDATE_CURRENT);
-			
 			// Assign the PendingIntent to the Notification Builder.
 			builder.setContentIntent(pendingIntent);
 			
@@ -95,16 +95,7 @@ public class MainActivity extends Activity
 		{
 			
 			NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-			
-			// Define the notification that will be displayed to the user in two forms:
-			// 1. via small icon (RADIO) in the notification area or status bar and
-			// 2. via the notification drawer when the user pulls it down.
-			Notification.Builder builder = new Notification.Builder(v.getContext());
-			builder.setSmallIcon(android.R.drawable.btn_radio);
-			builder.setContentTitle("Notifying All CS Students");
-			builder.setContentText("Displaying SPECIAL activity.");
-			builder.setSubText("Are you paying attention?");
-			
+
 			Intent notifyIntent = new Intent(v.getContext(), SpecialActivity.class);
 			notifyIntent.setFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 			
@@ -115,6 +106,14 @@ public class MainActivity extends Activity
 					                                                notifyIntent,  
 					                                                PendingIntent.FLAG_UPDATE_CURRENT);
 			
+			// Define the notification that will be displayed to the user in two forms:
+			// 1. via small icon (RADIO) in the notification area or status bar and
+			// 2. via the notification drawer when the user pulls it down.
+			Notification.Builder builder = new Notification.Builder(v.getContext());
+			builder.setSmallIcon(android.R.drawable.btn_radio);
+			builder.setContentTitle("Notifying All CS Students");
+			builder.setContentText("Displaying SPECIAL activity.");
+			builder.setSubText("Are you paying attention?");
 			// Assign the PendingIntent to the Notification Builder.
 			builder.setContentIntent(pendingIntent);
 			
@@ -131,15 +130,6 @@ public class MainActivity extends Activity
 			
 			NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 			
-			// Define the notification that will be displayed to the user in two forms:
-			// 1. via small icon (PLUS) in the notification area or status bar and
-			// 2. via the notification drawer when the user pulls it down.
-			Notification.Builder builder = new Notification.Builder(v.getContext());
-			builder.setSmallIcon(android.R.drawable.btn_plus);
-			builder.setContentTitle("Notifying All CS Students");
-			builder.setContentText("Displaying BACKSTACK activity.");
-			builder.setSubText("Are you paying attention?");
-			
 			// Build an artificial backstack to be used by the app if 
 			// you were going to show a different activity.
 			TaskStackBuilder stackBuilder = TaskStackBuilder.create(v.getContext());
@@ -149,8 +139,19 @@ public class MainActivity extends Activity
 			// Using example from developer site did not work, had to add a difference flag based on SO.
 			//  http://developer.android.com/guide/topics/ui/notifiers/notifications.html#DirectEntry
 			//  http://stackoverflow.com/questions/13148701/taskstackbuilder-and-extras-for-the-back-stack
-			PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_ONE_SHOT);
-			builder.setContentIntent(resultPendingIntent);
+			PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_ONE_SHOT);
+						
+						
+			// Define the notification that will be displayed to the user in two forms:
+			// 1. via small icon (PLUS) in the notification area or status bar and
+			// 2. via the notification drawer when the user pulls it down.
+			Notification.Builder builder = new Notification.Builder(v.getContext());
+			builder.setSmallIcon(android.R.drawable.btn_plus);
+			builder.setContentTitle("Notifying All CS Students");
+			builder.setContentText("Displaying BACKSTACK activity.");
+			builder.setSubText("Are you paying attention?");
+			// Assign the PendingIntent to the Notification Builder.
+			builder.setContentIntent(pendingIntent);
 			
 			// When using the above flag:  PendingIntent.FLAG_ONE_SHOT, it would only allow the 
 			// user to click the notification one anyway, so forcing cancel.
